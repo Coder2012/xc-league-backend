35a55d5ffa4d27e491a75968bf0feb83e16228a8
Neil Brown <neilbrown08@hotmail.com>
2019-07-20 23:00:54 +0100

    correct path to flights file

--- helpers/storage.js
+++ helpers/storage.js
@@ -1,6 +1,7 @@
 
 const aws = require('aws-sdk'); //require aws-sdk
 require('dotenv').config();
+const file = require('fs').createWriteStream('../../tmp/flights-2019.js');
 const s3 = new aws.S3({ accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET }); //create a s3 Object with s3 User ID and Key
 
 const read = async () => {
