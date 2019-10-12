7bff27b28d493e0d95c185c08d80d2f0f0f2298e
Neil Brown <neilbrown08@hotmail.com>
2019-10-12 14:24:27 +0100

    updating cors access control

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://www.xcleague.net,https://xcleague.net");
+  res.header("Access-Control-Allow-Origin", "https://www.xcleague.net");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
