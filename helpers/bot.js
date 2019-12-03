ce2c6253611a7c81b46288f557711f68c0bce743
Neil Brown <neilbrown08@hotmail.com>
2019-12-03 21:18:36 +0000

    refactor pre save to use Model.exists instead

--- helpers/bot.js
+++ helpers/bot.js
@@ -1,38 +1,34 @@
 const Model = require('./model');
 const Scraper = require('./scraper.js');
 
-const init = (flightUrls) => {
+const init = flightUrls => {
   let scraper = new Scraper(flightUrls);
 
-  scraper.events.on('error', function(error) {
+  scraper.events.on('error', error => {
     console.log('Scraper error: ', error);
     scraper.resume();
   });
 
-  scraper.events.on('complete', function(models) {
-    Model.schema.pre('save', function(next) {
-      var self = this;
-      Model.find({ identifier: self.identifier }, function(err, docs) {
-        if (docs === null || docs.length === 0) {
-          console.log('Saving', self.identifier);
-          next();
-        } else {
-          console.log('Flight exists: ', self.identifier);
-          next(new Error('Flight exists!'));
-        }
-      });
-    });
+  scraper.events.on('complete', async models => {
 
     for (let m of models) {
-      let model = new Model(m);
+      const model = new Model(m);
       console.log(`Pilot: ${model.pilot} - ${model.identifier}`);
-      model.save(function(err) {
-        if (err) {
-          console.log('Error saving: ' + model.identifier);
-        }else{
-          console.log('Saved', model.identifier)
-        }
-      });
+
+      const exists = await Model.exists({ identifier: m.identifier });
+      
+      if(!exists){
+        model.save(err => {
+          if (err) {
+            console.log(err);
+            console.log('Error saving: ' + model.identifier);
+          }else{
+            console.log('Saved', model.identifier)
+          }
+        });
+      }else{
+        console.log('Flight exists: ', model.identifier);
+      }
     }
 
     console.log('Saving complete');
