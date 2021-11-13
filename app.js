1e93e7e9862ca3ee0eb763e317599e4f6931cd06
Neil Brown <neilbrown08@hotmail.com>
2021-11-13 17:28:56 +0000

    try to fix cors

--- app.js
+++ app.js
@@ -55,10 +55,11 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested, Content-Type, Accept Authorization"
+  // );
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "POST, GET");
     return res.status(200).json({});
